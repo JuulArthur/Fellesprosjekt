c9ccd369b5cfa8600e8a7fd93baef2f1f469508f
Juul Arthur Ribe Rudihagen <juularthur92@gmail.com>
2013-03-06 09:29:41 +0100

    startet med databasen

--- server/src/database/CreateDatabase.java
+++ server/src/database/CreateDatabase.java
@@ -1,39 +1,55 @@
 package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CreateDatabase {
-  public static void main(String[] args) {
-    Connection connection = null;
-    Statement statement = null;
-    try {
-      Class.forName("org.gjt.mm.mysql.Driver").newInstance();
-      String url = "jdbc:mysql://localhost/mysql";
-      connection = DriverManager.getConnection(url, "username", "password");
 
-      statement = connection.createStatement();
-      String hrappSQL = "CREATE DATABASE hrapp";
-      statement.executeUpdate(hrappSQL);
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      if (statement != null) {
+    public static void main(String[] args) {
+
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "testuser";
+        String password = "test623";
+
         try {
-          statement.close();
-        } catch (SQLException e) {
-        } // nothing we can do
-      }
-      if (connection != null) {
-        try {
-          connection.close();
-        } catch (SQLException e) {
-        } // nothing we can do
-      }
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT VERSION()");
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(CreateDatabase.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(CreateDatabase.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
     }
-  }
-
 }
 
   
