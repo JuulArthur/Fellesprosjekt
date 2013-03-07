8cf19b499d93d1159e3813ad87e9f81b60168cf6
Juul Arthur Ribe Rudihagen <juularthur92@gmail.com>
2013-03-07 12:13:57 +0100

    lagt til mer på klient

---
+++ Klient/src/client/Client.java
@@ -0,0 +1,13 @@
+package client;
+
+public class Client extends BaseMessageClient {
+
+	public Client(String host, int port) throws Exception {
+		super( host, port );
+	}
+
+	public void onMessage( String message ) {
+		System.out.printf( "Message read is -> %s%n", message );
+	}
+
+}
---
+++ Klient/src/client/MessageClient.java
@@ -0,0 +1,9 @@
+package client;
+
+public interface MessageClient {
+
+	public void errorOnWrite( Exception e );
+	public void errorOnRead( Exception e );
+	public void onMessage( String message );
+
+}
