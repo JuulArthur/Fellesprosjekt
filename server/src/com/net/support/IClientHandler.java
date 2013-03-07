d366fdf129fa4e62e1a8438bcdc2d878a7dca19e
Per Øyvind Kanestrøm <perokane@gmail.com>
2013-03-07 14:13:33 +0100

    Removed some leftovers

--- server/src/com/net/support/IClientHandler.java
+++
@@ -1,9 +0,0 @@
-package com.net.support;
-
-public interface IClientHandler {
-
-	public void errorOnWrite( Exception e );
-	public void errorOnRead( Exception e );
-	public void onMessage( String message );
-
-}
