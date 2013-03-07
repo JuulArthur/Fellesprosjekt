5d6799ef9a840c27ad3411c87ae5ea3c0df560e4
Per Øyvind Kanestrøm <perokane@gmail.com>
2013-03-07 11:56:59 +0100

    More server stuff

--- server/src/com/net/ClientHandler.java
+++ server/src/com/net/ClientHandler.java
@@ -1,5 +1,25 @@
 package com.net;
 
-public class ClientHandler {
+import java.net.Socket;
+
+import com.net.support.BaseClientHandler;
+
+//import chat.support.BaseMessageClient;
+
+public class ClientHandler  extends BaseClientHandler {
+
+	private Server server;
+
+	public ClientHandler(Socket client, Server server) {
+		super(client);
+		this.server = server;
+	}
+
+	//@Override
+	public void onMessage(String message) {
+		System.out.println( "Got message from client:" );
+		System.out.println( message );
+		//this.server.onMessage(message, this);
+	}
 
 }
--- server/src/com/net/Server.java
+++ server/src/com/net/Server.java
@@ -1,5 +1,57 @@
 package com.net;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.net.ServerSocketFactory;
+
 public class Server {
+
+	private volatile boolean connected = true;
+	private List<ClientHandler> clients = Collections.synchronizedList(new ArrayList<ClientHandler>());
+	private int port;
+
+	public Server(int port) {
+		this.port = port;
+	}
+
+	public boolean isConnected() {
+		return connected;
+	}
+
+	public void setConnected(boolean connected) {
+		this.connected = connected;
+		if (!this.connected) {
+			for (ClientHandler client : this.clients) {
+				client.disconnect();
+			}
+		}
+	}
+
+	public void start() {
+		try {
+			ServerSocket server = ServerSocketFactory.getDefault()
+					.createServerSocket(this.port);
+			while (server.isBound() && this.isConnected()) {
+				Socket client = server.accept();
+				System.out.printf("Cliente conectado: %s%n", client.getInetAddress());
+				ClientHandler serverClient = new ClientHandler(client, this);
+				this.clients.add(serverClient);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void onMessage(String message, ClientHandler source) {
+		for (ClientHandler client : this.clients) {
+			if (!client.equals(source)) {
+				client.onMessage(message);
+			}
+		}
+	}
 
 }
