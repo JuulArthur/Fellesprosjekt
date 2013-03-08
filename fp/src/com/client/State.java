b02c2f7e18c7874dad241cd76fd2098f29eea275
Per Øyvind Kanestrøm <perokane@gmail.com>
2013-03-08 14:27:29 +0100

    States

---
+++ fp/src/com/client/State.java
@@ -0,0 +1,12 @@
+package com.client;
+
+/**
+ * The different states for the client
+ * @author perok
+ *
+ */
+public enum State {
+	Connecting, //Client needs to log in. Do not change if login fails.
+	Connected, //Client is logged in and can send command
+	Disconnected //Client has lost connection or timed out
+}
