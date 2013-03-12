1475c060e537c40b7408662807d5bcc866068335
Per Øyvind Kanestrøm <perokane@gmail.com>
2013-03-12 09:38:37 +0100

    We can log in :3

--- fp/src/com/server/net/ClientHandler.java
+++ fp/src/com/server/net/ClientHandler.java
@@ -1,6 +1,9 @@
 package com.server.net;
 
 import java.net.Socket;
+import java.sql.SQLException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import com.model.UserModel;
 import com.net.msg.MSGFlag;
@@ -19,6 +22,8 @@ import com.settings.Global;
  */
 public class ClientHandler  extends ServiceHandler {	
 	private Server server;
+	
+	private static final Executor POOL = Executors.newFixedThreadPool(16);
 
 	public ClientHandler(Socket client, Server server) {
 		super(client);
@@ -44,18 +49,36 @@ public class ClientHandler  extends ServiceHandler {
 				if(msgW.getFlag() == MSGFlag.LOGIN){
 					Object o = msgW.getObjects().get(0);
 					if(o instanceof UserModel){
+						if(Global.verbose) System.out.println("[ClientHandler] onWrapper: Logging in " + o);
+						
+						UserModel um = (UserModel)o;
 						/*
 						 * Query for login.
 						 * If query accepted, set the State to connected and send a RESPONSE back with acknowledge 
 						 */
-						if(Global.verbose) System.out.println("[ClientHandler] onWrapper: Logging in " + o);
 						
-						/* Set connected state*/
-						setState(State.CONNECTED);
+						boolean login = false;
 						
-						/* Send back an acknowledged state*/					
-						writeMessage(server.getJaxbMarshaller().getXMLRepresentation(msgW.getID(), MSGType.RESPONSE, MSGFlag.ACCEPT, null));
-	
+						try {
+							login = server.getFactory().checkPassword(um.getUsername(), um.getPassword());
+						} catch (ClassNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+						if(login){
+							/* Set connected state*/
+							setState(State.CONNECTED);
+							
+							/* Send back an acknowledged state*/					
+							writeMessage(server.getJaxbMarshaller().getXMLRepresentation(msgW.getID(), MSGType.RESPONSE, MSGFlag.ACCEPT, null));
+						}
+						else{
+							writeMessage(server.getJaxbMarshaller().getXMLRepresentation(msgW.getID(), MSGType.RESPONSE, MSGFlag.DECLINE, null));
+						}
 					}
 					else
 						System.out.println("[ERROR][ClientHandler] onWrapper: bad protocol");
@@ -78,3 +101,21 @@ public class ClientHandler  extends ServiceHandler {
 			
 	}
 }
+
+/*
+Runnable runnable = new Runnable() {
+	@Override
+	public void run() {
+		try {
+			login = server.getFactory().checkPassword(um.getUsername(), um.getPassword());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+};
+
+POOL.execute(runnable);*/
