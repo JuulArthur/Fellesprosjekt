86b1ae1e0c3fb3e436904dbf1e80fe28d849b70d
Juul Arthur Ribe Rudihagen <juularthur92@gmail.com>
2013-03-12 09:20:44 +0100

    tou can now get a user from the database

--- fp/src/com/server/db/Factory.java
+++ fp/src/com/server/db/Factory.java
@@ -13,17 +13,52 @@ public class Factory {
 		db = new DatabaseConnector();
 	}
 	
+	public void UpdateDatabase(String query) throws SQLException, ClassNotFoundException{
+		db.initialize();
+		db.makeUpdate(query);
+		db.close();
+	}
+	
 	public UserModel createUser(String username, String password, String email, 
 			String name, String surname, String phoneNumber, int isAdmin ) 
 					throws SQLException, ClassNotFoundException{
 		UserModel um = new UserModel(username, password, email, name, surname, phoneNumber, isAdmin);
 		String query=String.format("insert into User " +
 				"(userName, password, email, name, surName, phoneNumber, isAdmin) values ('%s', '%s','%s','%s','%s','%s',%d)", username, password, email, name, surname, phoneNumber, isAdmin); 
-		db.initialize();
-		db.makeUpdate(query);
-		db.close();
-	
+		UpdateDatabase(query);
 		return um;
+	}
+	
+	public UserModel getUserModel(String username) throws ClassNotFoundException, SQLException
+	{
+		
+		
+		String query=String.format("Select username,email,name,surname,phonenumber,password,isadmin from User where username='%s'",username);
+		db.initialize();
+		ResultSet rs=db.makeSingleQuery(query);
+		String password=null;
+		String email=null;
+		String name=null;
+		String surname=null;
+		String phoneNumber=null;
+		int isAdmin=0;
+		while(rs.next())
+		{
+			email=rs.getString(2);
+			name=rs.getString(3);
+			surname=rs.getString(4);
+			phoneNumber=rs.getString(5);
+			password=rs.getString(6);
+			isAdmin=rs.getInt(7);
+		}
+		
+		UserModel um = new UserModel(username, password, email, name, surname, phoneNumber, isAdmin);
+		rs.close();
+		db.close();
+		
+		return um ;
+	
+		
 	}
 	
 	public boolean checkPassword(String username, String password) 
