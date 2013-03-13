171ba94b67a156c3c23f26fd35d47ecc32da13ea
cristeahub <christoffer.tonnessen@gmail.com>
2013-03-13 13:58:46 +0100

    added createCalendar

--- fp/src/com/server/db/Factory.java
+++ fp/src/com/server/db/Factory.java
@@ -3,6 +3,7 @@ package com.server.db;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.Properties;
 
@@ -11,6 +12,7 @@ import javax.xml.bind.annotation.XmlElement;
 import com.model.AlarmModel;
 import com.model.AppointmentModel;
 import com.model.UserModel;
+import com.model.CalendarModel;
 
 public class Factory {
 
@@ -20,11 +22,65 @@ public class Factory {
 		db = new DatabaseConnector();
 	}
 
-	public void UpdateDatabase(String query) throws SQLException,
+	public void updateDatabase(String query) throws SQLException,
 			ClassNotFoundException {
 		db.initialize();
 		db.makeUpdate(query);
 		db.close();
+	}
+	
+	public CalendarModel createCalendarModel(CalendarModel cm) throws SQLException, ClassNotFoundException {
+		return createCalendarModel(cm.getId(), cm.getName(), cm.getAppointments(), cm.getOwner(), cm.getFollowers());
+	}
+
+	public CalendarModel createCalendarModel(int id, String name,
+			ArrayList<AppointmentModel> appointments, UserModel owner,
+			ArrayList<UserModel> followers) throws SQLException,
+			ClassNotFoundException {
+		PreparedStatement ps;
+		if (appointments == null)
+			appointments = new ArrayList<AppointmentModel>() {
+			};
+		if (followers == null)
+			followers = new ArrayList<UserModel>() {
+			};
+		CalendarModel cm = new CalendarModel(id, name, appointments, owner,
+				followers);
+		String query = String.format("insert into Calendar "
+				+ "(id, name) values ('%s', '%s')", id, name);
+		updateDatabase(query);
+		query = String.format("insert into Follows "
+				+ "(isOwner, username, calendarid) values ('%s', '%s', '%s')",
+				1, owner.getUsername(), id);
+		updateDatabase(query);
+		if (appointments.size() > 0) {
+			db.initialize();
+			ps = db.makeBatchUpdate(String.format("insert into BelongTo "
+					+ "(appointmentid, calendarid) values (?, '%s')", name));
+			for (int i = 0; i < appointments.size(); i++) {
+				ps.setInt(1, appointments.get(i).getId());
+				ps.addBatch();
+			}
+			System.out.println(ps.toString());
+			ps.executeBatch();
+			ps.close();
+			db.close();
+		}
+		if (followers.size() > 0) {
+			db.initialize();
+			ps = db.makeBatchUpdate(String.format("insert into Follows"
+					+ "(isOwner, username, calendarid) values ('%s', ?, '%s')",
+					0, name));
+			for (int i = 0; i < followers.size(); i++) {
+				ps.setString(1, followers.get(i).getName());
+				ps.addBatch();
+			}
+			System.out.println(ps.toString());
+			ps.executeBatch();
+			ps.close();
+			db.close();
+		}
+		return cm;
 	}
 
 	public UserModel createUserModel(String username, String password,
@@ -37,17 +93,19 @@ public class Factory {
 						+ "(userName, password, email, name, surName, phoneNumber, isAdmin) values ('%s', '%s','%s','%s','%s','%s',%d)",
 						username, password, email, name, surname, phoneNumber,
 						isAdmin);
-		UpdateDatabase(query);
+		updateDatabase(query);
 		return um;
 	}
-	
-	public UserModel createUserModel(UserModel um) throws SQLException, ClassNotFoundException {
+
+	public UserModel createUserModel(UserModel um) throws SQLException,
+			ClassNotFoundException {
 		String query = String
 				.format("insert into User "
 						+ "(userName, password, email, name, surName, phoneNumber, isAdmin) values ('%s', '%s','%s','%s','%s','%s',%d)",
-						um.getUsername(), um.getPassword(), um.getEmail(), um.getEmail(), um.getSurname(),
-						um.getPhoneNumber(), um.isAdmin());
-		UpdateDatabase(query);
+						um.getUsername(), um.getPassword(), um.getEmail(),
+						um.getEmail(), um.getSurname(), um.getPhoneNumber(),
+						um.isAdmin());
+		updateDatabase(query);
 		return um;
 	}
 
@@ -82,9 +140,9 @@ public class Factory {
 		return um;
 
 	}
-	
-	public UserModel getUserModel(UserModel um)
-			throws ClassNotFoundException, SQLException {
+
+	public UserModel getUserModel(UserModel um) throws ClassNotFoundException,
+			SQLException {
 
 		String query = String
 				.format("Select username,email,name,surname,phonenumber,password,isadmin from User where username='%s'",
@@ -106,8 +164,8 @@ public class Factory {
 			isAdmin = rs.getInt(7);
 		}
 
-		UserModel utUm = new UserModel(um.getUsername(), password, email, name, surname,
-				phoneNumber, isAdmin);
+		UserModel utUm = new UserModel(um.getUsername(), password, email, name,
+				surname, phoneNumber, isAdmin);
 		rs.close();
 		db.close();
 
@@ -122,30 +180,32 @@ public class Factory {
 				"UPDATE User SET email='%s',name='%s',surname='%s',phonenumber='%s'"
 						+ ",password='%s',isAdmin=%d WHERE username='%s'",
 				email, name, surname, phoneNumber, password, isAdmin, username);
-		UpdateDatabase(query);
+		updateDatabase(query);
 	}
-	
-	public void updateUserModel(UserModel um)
-			throws SQLException, ClassNotFoundException {
+
+	public void updateUserModel(UserModel um) throws SQLException,
+			ClassNotFoundException {
 		String query = String.format(
 				"UPDATE User SET email='%s',name='%s',surname='%s',phonenumber='%s'"
 						+ ",password='%s',isAdmin=%d WHERE username='%s'",
-				um.getEmail(), um.getName(), um.getSurname(), um.getPhoneNumber(), um.getPassword(), um.isAdmin(), um.getUsername());
-		UpdateDatabase(query);
+				um.getEmail(), um.getName(), um.getSurname(),
+				um.getPhoneNumber(), um.getPassword(), um.isAdmin(),
+				um.getUsername());
+		updateDatabase(query);
 	}
 
 	public void deleteUserModel(String username) throws SQLException,
 			ClassNotFoundException {
 		String query = String.format("DELETE from User WHERE username='%s'",
 				username);
-		UpdateDatabase(query);
+		updateDatabase(query);
 	}
-	
+
 	public void deleteUserModel(UserModel um) throws SQLException,
 			ClassNotFoundException {
 		String query = String.format("DELETE from User WHERE username='%s'",
 				um.getUsername());
-		UpdateDatabase(query);
+		updateDatabase(query);
 	}
 
 	public boolean checkPassword(String username, String password)
@@ -164,12 +224,13 @@ public class Factory {
 
 		return returnPassword.equals(password) && !returnPassword.equals("");
 	}
-	
-	public boolean checkPassword(UserModel um)
-			throws SQLException, ClassNotFoundException {
+
+	public boolean checkPassword(UserModel um) throws SQLException,
+			ClassNotFoundException {
 		String returnPassword = "";
 		String query = String.format(
-				"SELECT password FROM User WHERE username='%s'", um.getUsername());
+				"SELECT password FROM User WHERE username='%s'",
+				um.getUsername());
 		db.initialize();
 		ResultSet rs = db.makeSingleQuery(query);
 		rs.beforeFirst();
@@ -179,7 +240,8 @@ public class Factory {
 		rs.close();
 		db.close();
 
-		return returnPassword.equals(um.getPassword()) && !returnPassword.equals("");
+		return returnPassword.equals(um.getPassword())
+				&& !returnPassword.equals("");
 	}
 
 	public AlarmModel createAlarmModel(Date date, String text,
@@ -190,7 +252,7 @@ public class Factory {
 				+ "(date, text, appointmentid, username) values "
 				+ "('%s', '%s',%d,'%s')", date, text, ap.getId(),
 				user.getUsername());
-		UpdateDatabase(query);
+		updateDatabase(query);
 		return am;
 	}
 
@@ -200,7 +262,7 @@ public class Factory {
 				+ "(date, text, appointmentid, username) values "
 				+ "('%s', '%s',%d,'%s')", am.getDate(), am.getText(), am
 				.getAppointment().getId(), am.getCreator().getUsername());
-		UpdateDatabase(query);
+		updateDatabase(query);
 		return am;
 	}
 
@@ -225,13 +287,13 @@ public class Factory {
 
 		return am;
 	}
-	
+
 	public AlarmModel getAlarmModel(AlarmModel am)
 			throws ClassNotFoundException, SQLException {
 
 		String query = String.format("Select date, text "
-				+ "from Alarm WHERE username='%s'AND appointmentid=%d",
-				am.getCreator().getUsername(), am.getAppointment().getId());
+				+ "from Alarm WHERE username='%s'AND appointmentid=%d", am
+				.getCreator().getUsername(), am.getAppointment().getId());
 		db.initialize();
 		ResultSet rs = db.makeSingleQuery(query);
 		Date date = null;
@@ -241,7 +303,8 @@ public class Factory {
 			text = rs.getString(2);
 		}
 
-		AlarmModel utAm = new AlarmModel(date, text, am.getAppointment(), am.getCreator());
+		AlarmModel utAm = new AlarmModel(date, text, am.getAppointment(),
+				am.getCreator());
 		rs.close();
 		db.close();
 
@@ -250,19 +313,22 @@ public class Factory {
 
 	public void updateAlarmModel(Date date, String text, AppointmentModel ap,
 			UserModel user) throws SQLException, ClassNotFoundException {
-		String query = String.format(
-				"UPDATE Alarm SET date='%s',text='%s' WHERE appointmentid=%d AND username='%s'",
-				date, text, ap.getId());
-		UpdateDatabase(query);
+		String query = String
+				.format("UPDATE Alarm SET date='%s',text='%s' WHERE appointmentid=%d AND username='%s'",
+						date, text, ap.getId());
+		updateDatabase(query);
 
 	}
 
-	public void updateAlarmModel(AlarmModel am) throws SQLException, ClassNotFoundException {
+	public void updateAlarmModel(AlarmModel am) throws SQLException,
+			ClassNotFoundException {
 		System.out.println(am.getDate());
-		String query = String.format(
-				"UPDATE Alarm SET date='%s',text='%s' WHERE appointmentid=%d AND username='%s'",
-				am.getDate(), am.getText(), am.getAppointment().getId(), am.getCreator().getUsername());
-		UpdateDatabase(query);
+		String query = String
+				.format("UPDATE Alarm SET date='%s',text='%s' WHERE appointmentid=%d AND username='%s'",
+						am.getDate(), am.getText(),
+						am.getAppointment().getId(), am.getCreator()
+								.getUsername());
+		updateDatabase(query);
 
 	}
 
@@ -271,26 +337,28 @@ public class Factory {
 		String query = String.format(
 				"DELETE from Alarm WHERE username='%s' AND appointmentid=%d",
 				user.getUsername(), ap.getId());
-		UpdateDatabase(query);
+		updateDatabase(query);
 	}
-	
-	public void deleteAlarmModel(AlarmModel am)
-			throws SQLException, ClassNotFoundException {
+
+	public void deleteAlarmModel(AlarmModel am) throws SQLException,
+			ClassNotFoundException {
 		String query = String.format(
 				"DELETE from Alarm WHERE username='%s' AND appointmentid=%d",
 				am.getCreator().getUsername(), am.getAppointment().getId());
-		UpdateDatabase(query);
+		updateDatabase(query);
 	}
-	
+
 	/* APPOINTMENT */
-	//GET
-	public AppointmentModel getAppointmentModel(int pid) throws SQLException, ClassNotFoundException{
-		
-		String query=String.format("Select startTime, EndTime, host, title, text, place, isDeleted, date" +
-				"from Appointment WHERE id='%s'",pid);
-		
+	// GET
+	public AppointmentModel getAppointmentModel(int pid) throws SQLException,
+			ClassNotFoundException {
+
+		String query = String.format(
+				"Select startTime, EndTime, host, title, text, place, isDeleted, date"
+						+ "from Appointment WHERE id='%s'", pid);
+
 		db.initialize();
-		ResultSet rs=db.makeSingleQuery(query);
+		ResultSet rs = db.makeSingleQuery(query);
 
 		int startTime = 0;
 		int endTime = 0;
@@ -298,47 +366,47 @@ public class Factory {
 		String title = null;
 		String text = null;
 		String place = null;
-		boolean isDeleted; 
+		boolean isDeleted;
 		Date date = null;
 		ArrayList<UserModel> members = null;
-		
-		while(rs.next())
-		{
-			startTime=rs.getInt(2);
-			endTime =rs.getInt(3);
+
+		while (rs.next()) {
+			startTime = rs.getInt(2);
+			endTime = rs.getInt(3);
 			host = getUserModel(rs.getString(4));
-			title=rs.getString(5);
-			text=rs.getString(6);
-			place=rs.getString(7);
-			isDeleted=rs.getBoolean(8);
+			title = rs.getString(5);
+			text = rs.getString(6);
+			place = rs.getString(7);
+			isDeleted = rs.getBoolean(8);
 			date = rs.getDate(9);
 		}
-		
+
 		/* Get members */
-//		members = new ArrayList<>();
-		
-		query=String.format("Select username" +
-				"from IsSummonedTo WHERE appointmentid='%s'",pid);
-		rs=db.makeSingleQuery(query);
-		
-		while(rs.next()){
+		// members = new ArrayList<>();
+
+		query = String.format("Select username"
+				+ "from IsSummonedTo WHERE appointmentid='%s'", pid);
+		rs = db.makeSingleQuery(query);
+
+		while (rs.next()) {
 			members.add(getUserModel(rs.getString(1)));
 		}
-		
-		if(members.size() == 0)
+
+		if (members.size() == 0)
 			members = null;
-		
-		
+
 		rs.close();
 		db.close();
-		
-		return new AppointmentModel(pid, startTime, endTime, host, title, text, place, date, members);
+
+		return new AppointmentModel(pid, startTime, endTime, host, title, text,
+				place, date, members);
 	}
-	//UPDATE
-	//CREATE
-	
-	public void createAppointmentModel(AppointmentModel apModel){
-		
+
+	// UPDATE
+	// CREATE
+
+	public void createAppointmentModel(AppointmentModel apModel) {
+
 	}
-	//DELETE
+	// DELETE
 }
--- fp/src/com/server/db/TestDB.java
+++ fp/src/com/server/db/TestDB.java
@@ -9,130 +9,153 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 import com.model.AlarmModel;
 import com.model.AppointmentModel;
+import com.model.CalendarModel;
 import com.model.UserModel;
 
 public class TestDB {
-	
-	public void test() throws SQLException, ClassNotFoundException{
+
+	public void test() throws SQLException, ClassNotFoundException {
 		DatabaseConnector db = new DatabaseConnector();
-		
+
 		String insert = "insert into Groupp (id, groupName) values (2,'test');";
 		db.initialize();
 		db.makeUpdate(insert);
 		db.close();
 	}
-	
-	public void readTest() throws SQLException, ClassNotFoundException{
+
+	public void readTest() throws SQLException, ClassNotFoundException {
 		DatabaseConnector db = new DatabaseConnector();
-		
+
 		String sql = "select username,password from User WHERE username='perok'";
 		db.initialize();
 		ResultSet rs = db.makeSingleQuery(sql);
 		rs.beforeFirst();
-		while(rs.next()){
+		while (rs.next()) {
 			String name = rs.getString(1);
 			String password = rs.getString(2);
-			System.out.println(String.format("%S %S\n",name,password));
+			System.out.println(String.format("%S %S\n", name, password));
 		}
 		rs.close();
 	}
-	
-	public void batchTest() throws SQLException, ClassNotFoundException{
+
+	public void batchTest() throws SQLException, ClassNotFoundException {
 		DatabaseConnector db = new DatabaseConnector();
 		db.initialize();
 		String insert = "insert into Groupp (id, groupName) values (?,?);";
 		PreparedStatement ps = db.makeBatchUpdate(insert);
-		String id =null;
-		String groupName =null;
+		String id = null;
+		String groupName = null;
 		Scanner sc = new Scanner(System.in);
 		String line = sc.nextLine();
-		while(line.trim().length()>2){
+		while (line.trim().length() > 2) {
 			StringTokenizer t = new StringTokenizer(line);
-			id=t.nextToken();
-			groupName=t.nextToken();
+			id = t.nextToken();
+			groupName = t.nextToken();
 			ps.setInt(1, Integer.parseInt(id));
 			ps.setString(2, groupName);
 			ps.addBatch();
-			line=sc.nextLine();
+			line = sc.nextLine();
 		}
 		System.out.println(ps.toString());
 		ps.executeBatch();
 		ps.close();
 		db.close();
 	}
-	
-	public UserModel testCreateUser(String userName, String password, String email, 
-			String name, String surname, String phoneNumber, int isAdmin ) throws SQLException, ClassNotFoundException{
+
+	public UserModel testCreateUser(String userName, String password,
+			String email, String name, String surname, String phoneNumber,
+			int isAdmin) throws SQLException, ClassNotFoundException {
 		Factory f = new Factory();
-		UserModel um = f.createUserModel(userName, password, email, 
-		 name, surname, phoneNumber, isAdmin );
-				return um;
-		
+		UserModel um = f.createUserModel(userName, password, email, name,
+				surname, phoneNumber, isAdmin);
+		return um;
+
 	}
-	
-	public void testCreateAlarm() throws SQLException, ClassNotFoundException{
-		UserModel user = new UserModel("jarudiha", "hei", "hei", 
-				"Juul", "Rudihagen", "88888888", 1 );
+
+	public void testCreateAlarm() throws SQLException, ClassNotFoundException {
+		UserModel user = new UserModel("jarudiha", "hei", "hei", "Juul",
+				"Rudihagen", "88888888", 1);
 		Date date = new Date(2, 3, 4);
-		AppointmentModel ap = new AppointmentModel(1, 4, 5, user,
-			"sdf", "gdf", "haa", date, null);
+		AppointmentModel ap = new AppointmentModel(1, 4, 5, user, "sdf", "gdf",
+				"haa", date, null);
 		Factory f = new Factory();
 		AlarmModel am = new AlarmModel(date, "nidda", ap, user);
 		f.createAlarmModel(am);
 	}
-	
+
 	public void testGetAlarm() throws ClassNotFoundException, SQLException {
 		Factory f = new Factory();
 		AppointmentModel apm = new AppointmentModel();
-		UserModel user = new UserModel("perok", "hei", "hei", 
-				"Juul", "Rudihagen", "88888888", 1 );
+		UserModel user = new UserModel("perok", "hei", "hei", "Juul",
+				"Rudihagen", "88888888", 1);
 		Date date = new Date(0, 0, 0);
-		AppointmentModel ap = new AppointmentModel(2, 4, 5, user,
-			"sdf", "gdf", "haa", date, null);
+		AppointmentModel ap = new AppointmentModel(2, 4, 5, user, "sdf", "gdf",
+				"haa", date, null);
 		AlarmModel am = new AlarmModel(date, "", ap, user);
 		System.out.println(f.getAlarmModel(am));
 	}
-	
+
 	public void testUpdateAlarm() throws ClassNotFoundException, SQLException {
 		Factory f = new Factory();
 		AppointmentModel apm = new AppointmentModel();
-		UserModel user = new UserModel("perok", "hei", "hei", 
-				"Juul", "Rudihagen", "88888888", 1 );
+		UserModel user = new UserModel("perok", "hei", "hei", "Juul",
+				"Rudihagen", "88888888", 1);
 		Date date = new Date(0, 0, 0);
-		AppointmentModel ap = new AppointmentModel(2, 4, 5, user,
-			"sdf", "gdf", "haa", date, null);
+		AppointmentModel ap = new AppointmentModel(2, 4, 5, user, "sdf", "gdf",
+				"haa", date, null);
 		AlarmModel am = new AlarmModel(date, "NEI", ap, user);
 		f.updateAlarmModel(am);
 	}
-	
+
 	public void testDeleteAlarm() throws ClassNotFoundException, SQLException {
 		Factory f = new Factory();
 		AppointmentModel apm = new AppointmentModel();
-		UserModel user = new UserModel("perok", "hei", "hei", 
-				"Juul", "Rudihagen", "88888888", 1 );
+		UserModel user = new UserModel("perok", "hei", "hei", "Juul",
+				"Rudihagen", "88888888", 1);
 		Date date = new Date(0, 0, 0);
-		AppointmentModel ap = new AppointmentModel(2, 4, 5, user,
-			"sdf", "gdf", "haa", date, null);
+		AppointmentModel ap = new AppointmentModel(2, 4, 5, user, "sdf", "gdf",
+				"haa", date, null);
 		AlarmModel am = new AlarmModel(date, "NEI", ap, user);
 		f.deleteAlarmModel(am);
 	}
-	
-	public static void main(String args[]){
+
+	public void testMakeCal() throws ClassNotFoundException, SQLException {
+		Factory f = new Factory();
+		CalendarModel c = f.createCalendarModel(1, "chris",
+
+		new ArrayList<AppointmentModel>( Arrays.asList(
+			new AppointmentModel(),
+			new AppointmentModel()
+		)),
+
+		new UserModel("cristea", "ep", "chris", "tonn", "88888888",
+				"hei123", 1),
+						
+		new ArrayList<UserModel>( Arrays.asList(
+			new UserModel(),
+			new UserModel(),
+			new UserModel()
+		))
+		);
+	}
+
+	public static void main(String args[]) {
 		TestDB t = new TestDB();
-		try{
-			t.testDeleteAlarm();
-//		}
-//		catch(FileNotFoundException e){
-//			e.getStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
+		try {
+			t.testMakeCal();
+			// }
+			// catch(FileNotFoundException e){
+			// e.getStackTrace();
+			// } catch (IOException e) {
+			// // TODO Auto-generated catch block
+			// e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
