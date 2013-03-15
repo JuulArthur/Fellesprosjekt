eb4c6efdbeae748596e60ffa886928eca5d7bd6d
Julian Lam <jhlam@stud.ntnu.no>
2013-03-15 14:21:40 +0100

    fiksa mooterom. ser awesome ut

--- fp/src/com/view/MeetingRoomPanel.java
+++ fp/src/com/view/MeetingRoomPanel.java
@@ -19,6 +19,8 @@ import javax.swing.JTextField;
 public class MeetingRoomPanel extends JPanel {
 
 	private JFrame meetingRoomFrame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	public MeetingRoomPanel () {
 		/*
@@ -29,9 +31,9 @@ public class MeetingRoomPanel extends JPanel {
 		 */
 			
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0};
 		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 62, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
@@ -48,7 +50,7 @@ public class MeetingRoomPanel extends JPanel {
 		btnReturn.addActionListener(new returnAction());
 		GridBagConstraints gbc_btnReturn = new GridBagConstraints();
 		gbc_btnReturn.insets = new Insets(0, 0, 5, 0);
-		gbc_btnReturn.gridx = 4;
+		gbc_btnReturn.gridx = 3;
 		gbc_btnReturn.gridy = 0;
 		add(btnReturn, gbc_btnReturn);
 		
@@ -74,12 +76,21 @@ public class MeetingRoomPanel extends JPanel {
 		gbc_lblStart.gridy = 2;
 		add(lblStart, gbc_lblStart);
 		
-		JSpinner spinner_1 = new JSpinner();
-		GridBagConstraints gbc_spinner_1 = new GridBagConstraints();
-		gbc_spinner_1.insets = new Insets(0, 0, 5, 5);
-		gbc_spinner_1.gridx = 2;
-		gbc_spinner_1.gridy = 2;
-		add(spinner_1, gbc_spinner_1);
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 2;
+		add(textField, gbc_textField);
+		textField.setColumns(5);
+		
+		JLabel lblNewLabel_1 = new JLabel("(tt:mm)");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_1.gridx = 3;
+		gbc_lblNewLabel_1.gridy = 2;
+		add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		JLabel lblNewLabel = new JLabel("Slutt");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
@@ -89,12 +100,21 @@ public class MeetingRoomPanel extends JPanel {
 		gbc_lblNewLabel.gridy = 3;
 		add(lblNewLabel, gbc_lblNewLabel);
 		
-		JSpinner spinner_2 = new JSpinner();
-		GridBagConstraints gbc_spinner_2 = new GridBagConstraints();
-		gbc_spinner_2.insets = new Insets(0, 0, 5, 5);
-		gbc_spinner_2.gridx = 2;
-		gbc_spinner_2.gridy = 3;
-		add(spinner_2, gbc_spinner_2);
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 3;
+		add(textField_1, gbc_textField_1);
+		textField_1.setColumns(5);
+		
+		JLabel lblNewLabel_2 = new JLabel("(tt:mm)");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_2.gridx = 3;
+		gbc_lblNewLabel_2.gridy = 3;
+		add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
 		JLabel lblLedigeRom = new JLabel("Ledige rom");
 		GridBagConstraints gbc_lblLedigeRom = new GridBagConstraints();
