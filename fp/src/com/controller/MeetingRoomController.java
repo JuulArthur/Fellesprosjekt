05babe9abe75d09528090504ed0dfa113288b0b0
Per Øyvind Kanestrøm <perokane@gmail.com>
2013-03-21 10:50:41 +0100

    derp

--- fp/src/com/controller/MeetingRoomController.java
+++ fp/src/com/controller/MeetingRoomController.java
@@ -33,129 +33,100 @@ public class MeetingRoomController implements ActionListener, IServerResponse {
 	private AppointmentModel appointment;
 	private MainMeetingPanel meeting;
 	private ArrayList<Object> alist = new ArrayList<Object>();
-	
-	private DefaultListModel<RoomModel> defaultListModel;
 
-	
+	private DefaultListModel<RoomModel> defaultListModel;
 
 	public MeetingRoomController(MainGUI gui, MainMeetingPanel meeting) {
 		this.gui = gui;
 		this.view = new MeetingRoomPanel();
-		//this.appointment = appointment;
+		// this.appointment = appointment;
 		this.meeting = meeting;
 		defaultListModel = new DefaultListModel<RoomModel>();
-		
-		view.setStartText(""+meeting.getStartText());
-		view.setEndText(""+meeting.getEndText());
-		
-		view.getSearch().addActionListener(this);
-		
-		view.addWindowListener(new WindowAdapter() 
-		{
-		  public void windowClosed(WindowEvent e)
-		  {
-		    
-		  }
 
-		  public void windowClosing(WindowEvent e)
-		  {
-		    Global.respondGUI.remove(this);
-		  }
+		view.setStartText("" + meeting.getStartText());
+		view.setEndText("" + meeting.getEndText());
+
+		view.getSearch().addActionListener(this);
+
+		view.addWindowListener(new WindowAdapter() {
+			public void windowClosed(WindowEvent e) {
+
+			}
+
+			public void windowClosing(WindowEvent e) {
+				Global.respondGUI.remove(this);
+			}
 		});
-		
+
 		view.getRoomList().setCellRenderer(new RoomListRenderer());
-		
+
 		view.setLocationRelativeTo(gui);
-		
-		view.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);	
+
+		view.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		view.setVisible(true);
 
-		
 	}
-	
 
 	public boolean recievedObjectRespone(boolean success, ArrayList<Object> al) {
-		if (success) {		
+		if (success) {
 			defaultListModel = new DefaultListModel<RoomModel>();
-			for(Object o : al)
-				defaultListModel.addElement((RoomModel)o);
-			
+			for (Object o : al)
+				defaultListModel.addElement((RoomModel) o);
+
 			view.getRoomList().setModel(defaultListModel);
-			}
-			
+		}
+
 		return false;
 	}
 
-	
 	public void actionPerformed(ActionEvent e) {
-		
-		if (e.getSource() == view.getSearch()){
-			//hente ut en liste over ledige rom ut fra kapasitet og tidspunkt
+
+		if (e.getSource() == view.getSearch()) {
+			// hente ut en liste over ledige rom ut fra kapasitet og tidspunkt
 			alist.add(view.getCapacity());
 			alist.add(createNumValue(meeting.getStartText()));
 			alist.add(createNumValue(meeting.getEndText()));
 			Global.sHandler.setCurrentFlag(MSGFlagVerb.GET);
 			Global.sHandler.setState(State.CONNECTED_WAITING);
-			Global.sHandler.writeMessage(Global.jaxbMarshaller.getXMLRepresentation(0, MSGType.REQUEST, MSGFlagVerb.GET, MSGFlagSubject.AVAILIBLEROOM, alist));
-		
-			}
-			
-		
-		
-		else if (e.getSource() == view.getChooseRoom()){
-			//legge til valgte rom i appointmentModel og g� tilbake til MeetingPanel
-			appointment.setPlace((String) view.getRoomList().getSelectedValue());
-			
-			
-			
+			Global.sHandler.writeMessage(Global.jaxbMarshaller
+					.getXMLRepresentation(0, MSGType.REQUEST, MSGFlagVerb.GET,
+							MSGFlagSubject.AVAILIBLEROOM, alist));
+
+		} else if (e.getSource() == view.getChooseRoom()) {
+			// legge til valgte rom i appointmentModel og g� tilbake til
+			// MeetingPanel
+			appointment
+					.setPlace((String) view.getRoomList().getSelectedValue());
 		}
-		
-		
-		
 	}
-	
-	private int createNumValue(String s){
-		String  b[] = s.split("\\:");
+
+	private int createNumValue(String s) {
+		String b[] = s.split("\\:");
 
 		int a = Integer.parseInt(b[0]);
 		a *= 60;
 		a += Integer.parseInt(b[1]);
-		
+
 		return a;
 	}
 
-
 }
 
-class RoomListRenderer extends DefaultListCellRenderer{
-	
-	//private static ImageIcon maleIcon;
-	//private static ImageIcon femaleIcon;
-	
-	@Override 
-	public Component getListCellRendererComponent( JList list, Object value, int index, boolean isSelected, boolean cellHasFocus ) {  
-        JLabel lbl = (JLabel)super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus );  
-        
-        /*
-        if(maleIcon == null){
-        	maleIcon = new ImageIcon(new ImageIcon("img/Comics-Hero-Red-icon.png").getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH));
-        	femaleIcon = new ImageIcon(new ImageIcon("img/Street-Fighter-Chun-Li-icon.png").getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH));
-        }
-        */
-        
-        RoomModel room = (RoomModel)value;
-        
-        /*
-        if(person.getGender() == Gender.female)      
-            lbl.setIcon(femaleIcon);
-        else
-            lbl.setIcon(maleIcon);
-            */
-		
-		lbl.setText("<html><b>" + room.getRoomName() + "</b></html>");//<br/><i>" + calendar.getOwner() +"</i></html>");
+class RoomListRenderer extends DefaultListCellRenderer {
+
+	// private static ImageIcon maleIcon;
+	// private static ImageIcon femaleIcon;
+
+	@Override
+	public Component getListCellRendererComponent(JList list, Object value,
+			int index, boolean isSelected, boolean cellHasFocus) {
+		JLabel lbl = (JLabel) super.getListCellRendererComponent(list, value,
+				index, isSelected, cellHasFocus);
+
+		RoomModel room = (RoomModel) value;
+
+		lbl.setText("<html><b>" + room.getRoomName() + "</b></html>");// <br/><i>" + calendar.getOwner() +"</i></html>");
 
 		return lbl;
 	}
 }
-
-	
