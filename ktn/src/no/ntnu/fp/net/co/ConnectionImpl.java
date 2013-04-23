c7739b94b1f9a10b06d3281857ddfae7e0694650
Per Øyvind Kanestrøm <perokane@gmail.com>
2013-04-23 15:25:07 +0200

    Stud.ass approved

--- ktn/src/no/ntnu/fp/net/co/ConnectionImpl.java
+++ ktn/src/no/ntnu/fp/net/co/ConnectionImpl.java
@@ -337,12 +337,7 @@ public class ConnectionImpl extends AbstractConnection {
 	private int counter = 10;
 	private final static int max_count = 4;
 	public void close() throws IOException {
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+
 		System.out.println("close happening");
 		System.out.println(this.state);
 		
@@ -426,6 +421,12 @@ public class ConnectionImpl extends AbstractConnection {
 		}
 		/* CLIENT*/
 		else if(this.state == State.CLOSE_WAIT){
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			KtnDatagram finPacket = constructInternalPacket(Flag.FIN);
 			try {
 				simplySendPacket(finPacket);
